7361c4ce381fa2875b32cfb5f5c5490d3bfe7a93
Ammaron <ammaron99@gmail.com>
2025-07-23 17:13:18 -0500

    first picture album

--- week02/top10/scripts/script.js
+++ week02/top10/scripts/script.js
@@ -2,7 +2,28 @@ const chapter = document.querySelector('#favchap');
 const submitButton = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const faveChap = document.createElement('li');
+// const faveChap = document.createElement('li');
 
 
-const deleteButton = article.querySelector('❌');
+// const deleteButton = document.createElement('button');
+
+submitButton.addEventListener('click', function () {
+    const faveChap = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    if (chapter.value.trim() !== '') {
+        faveChap.textContent = chapter.value;
+        deleteButton.textContent = '❌';
+        faveChap.append(deleteButton);
+        list.append(faveChap);
+        chapter.value = '';
+        chapter.focus();
+    }
+
+});
+
+list.addEventListener('click', function (removeSelection) {
+
+    if (removeSelection.target.textContent == '❌') {
+        removeSelection.target.parentElement.remove();
+    }
+});
