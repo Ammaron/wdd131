1c07e9ee669b01b431109514ee4895659e557d9f
Kirby McDonald <ammaron99@gmail.com>
2025-07-23 11:28:56 -0500

    again

---
+++ week02/top10/scripts/script.js
@@ -0,0 +1,8 @@
+const chapter = document.querySelector('#favchap');
+const submitButton = document.querySelector('button');
+const list = document.querySelector('#list');
+
+const faveChap = document.createElement('li');
+
+
+const deleteButton = article.querySelector('❌');
