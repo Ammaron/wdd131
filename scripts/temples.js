1f97128658c06364846509cf4335e8c500ca408f
Ammaron <ammaron99@gmail.com>
2025-07-25 18:04:12 -0500

    9

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,11 @@
 document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const menu = document.getElementById('menu');
+const temple = document.querySelector('.temple')
+
+hamburgerMenu.addEventListener('click', function () {
+    menu.classList.toggle('show');
+    temple.classList.toggle('hide')
+
+});
